bb276d3183290c1265d9319ec21601b9e1f8f8d1
Lisa Chen <fslisa48@yahoo.ca>
2024-04-16 02:29:37 +0000

    W3D1

--- demo.rb
+++ demo.rb
@@ -1,5 +1,11 @@
-def sum(a,b)
-    puts a+b
-end
-
-sum(1,5)
+for i in (1..100)
+    if i % 3 == 0 && i % 5 == 0
+        puts "FizzBuzz"
+    elsif i % 3 == 0 
+            puts "Fizz"
+    elsif i % 5 == 0
+        puts "Buzz"
+    else
+        puts i
+    end
+end                                      
