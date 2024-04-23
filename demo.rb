dcc252186fdf04cc7440e22db32957c59c7dbfca
Lisa Chen <fslisa48@yahoo.ca>
2024-04-23 02:43:05 +0000

    W4D1

--- demo.rb
+++ demo.rb
@@ -1,11 +1,20 @@
-for i in (1..100)
-    if i % 3 == 0 && i % 5 == 0
-        puts "FizzBuzz"
-    elsif i % 3 == 0 
-            puts "Fizz"
-    elsif i % 5 == 0
-        puts "Buzz"
-    else
-        puts i
+class Human
+    def walk
+        puts "I can walk!"
     end
-end                                      
+
+    def run
+        puts "I can run!"
+    end
+end
+
+class SuperHuman < Human  #SuperHuman inherit all qualities of SuperHuman
+    def fast_walk
+        puts "I can walk very fast!"
+    end
+end
+
+Lisa = SuperHuman.new
+
+Lisa.walk
+Lisa.fast_walk
